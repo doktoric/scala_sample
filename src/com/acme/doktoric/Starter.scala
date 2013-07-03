e298e33855e50414207f432caec56707dea17586
Richard Doktorics <doktoricsrichard@gmail.com>
2013-07-03 21:18:45 +0200

    init

---
+++ src/com/acme/doktoric/Starter.scala
@@ -0,0 +1,14 @@
+package com.acme.doktoric
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ricsi
+ * Date: 2013.07.03.
+ * Time: 21:01
+ * To change this template use File | Settings | File Templates.
+ */
+object main {
+  def main(args: Array[String]) {
+    println("First Scala app!")
+  }
+}
