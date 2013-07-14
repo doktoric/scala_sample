b8a48e29180045ac2b439e32d49c15b123675b76
Richard Doktorics <doktoricsrichard@gmail.com>
2013-07-14 18:38:10 +0200

    iterating on a list

--- src/com/acme/doktoric/Starter.scala
+++ src/com/acme/doktoric/Starter.scala
@@ -12,6 +12,34 @@ object main {
     val msg = "Hello world"
     println(msg)
     println(max(3,6))
+    var sampleList= List(1,2,3)
+    writeListToConsoleWithFor(sampleList)
+    writeListToConsoleWithWhile(sampleList)
+    writeListToConsoleWithForEach(sampleList)
+
+  }
+
+  def writeListToConsoleWithFor(x: List[Int]):Unit = {
+    println("writeListToConsoleWithFor:")
+    for (i<- 0.to(x.length-1))
+      println(x.apply(i))
+  }
+
+  def writeListToConsoleWithForEach(x: List[Int]):Unit = {
+    println("writeListToConsoleWithForEach:")
+    for (i<-x)
+      println(i)
+    println("or writeListToConsoleWithForEach:")
+    x.foreach(println)
+  }
+
+  def writeListToConsoleWithWhile(x: List[Int]):Unit = {
+    var i=0
+    println("writeListToConsoleWithWhile:")
+    while ( i< x.length) {
+      println(x(i))
+      i+=1;
+    }
   }
 
   def max(x: Int, y: Int): Int = {
