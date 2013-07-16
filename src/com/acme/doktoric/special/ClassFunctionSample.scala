40e6826e4e611f5836637364144c7f85077441fc
Richard Doktorics <doktoricsrichard@gmail.com>
2013-07-16 19:48:10 +0200

    classfunction

---
+++ src/com/acme/doktoric/special/ClassFunctionSample.scala
@@ -0,0 +1,12 @@
+package com.acme.doktoric.special
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ricsi
+ * Date: 2013.07.15.
+ * Time: 22:14
+ * To change this template use File | Settings | File Templates.
+ */
+class ClassFunctionSample {
+  var increase = (x: Int) => x + 1
+}
