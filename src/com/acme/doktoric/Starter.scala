7eed53a90a20c3488198195fa48d7904151f3d90
Richard Doktorics <doktoricsrichard@gmail.com>
2013-08-28 21:35:34 +0200

    case class sample

--- src/com/acme/doktoric/Starter.scala
+++ src/com/acme/doktoric/Starter.scala
@@ -1,5 +1,6 @@
 package com.acme.doktoric
 
+import cc.Card
 import classes.{SpecificDog, LittleDog, BigDog}
 import iteration.CollectionSample
 import matcher.MatcherSample
@@ -34,6 +35,9 @@ object main {
     println(classFunctionSample.writeNumbersToConsole(List(1,2,3)))
     println(classFunctionSample.increaseWithPrint(1))
     println("curriedSum: " + classFunctionSample.curriedSum(1)(2))
+
+    val rimmer = name(new Card(1))
+    println(rimmer)
   }
 
 
@@ -41,5 +45,13 @@ object main {
   def max(x: Int, y: Int): Int = {
     if (x > y) x
     else y
+  }
+
+  def name(c:Card) = c match {
+    case Card(1) => "Ace"
+    case Card(11) => "Jack" // Or Knave, depending on preference
+    case Card(12) => "Queen"
+    case Card(13) => "King"
+    case Card(x:Int) => x.toString()
   }
 }
---
+++ src/com/acme/doktoric/cc/Card.scala
@@ -0,0 +1,12 @@
+package com.acme.doktoric.cc
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ricsi
+ * Date: 2013.08.28.
+ * Time: 21:11
+ * To change this template use File | Settings | File Templates.
+ */
+case class Card(value:Int)  {
+
+}
